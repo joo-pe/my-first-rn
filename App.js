29c58eb4556a516a0104da299cf887ca1dc3ced7
jaden <joohyung1002@naver.com>
2022-10-07 13:45:37 +0900

    environment setting

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const name = "joo";
+  const add = (a, b) => a + b;
+  const isFullname = false;
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      {/*  주석 */}
+      <View style={styles.container}>
+        {/* <Text>My name {name} </Text> */}
+        <Text>1 + 2 = {add(1, 2)} </Text>
+        <Text>{isFullname === true ? name + "Kim" : name} </Text>
+        <StatusBar style="auto" />
+      </View>
+      <Text>show joo</Text>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
